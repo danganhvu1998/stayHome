39ac98fa55cfa1bfc16a72d235af58bf1ff9bd31
danganhvu1998 <danganhvu1998@gmail.com>
2018-09-22 23:39:02 +0900

    avata rule update

--- server/resources/views/docsCtrl/eng.blade.php
+++ server/resources/views/docsCtrl/eng.blade.php
@@ -9,7 +9,7 @@
     <p>First: Register!</p>
     <p class="text-primary">This is how the thing done: First you add something to your buy list. Then go to <b>My Stuffs</b> to confirm that you really want to buy it. After that, other from the same building can see your order. They will take it of they want, go to the market, buy your stuffs and bring it home. After finish, they can mark order as finished. After that, you can see who and which room is now having your stuffs and go to take it. Of course you have to pay money according to bills. Finally, don't forget to <b>CONFIRM THAT YOU DO RECEIVE YOUR ORDER OR NOT</b></p>
     <p>If you want to go to supermaket, go to <b>Let's Go Out</b> and take some others' order with you. After finish it, you will gain point. The more you take, the more you get!</p>
-    <p>After that, you now can buy and request, <b>but nobody can see it!</b> You have to fill other information like <strong>room number</strong> or <strong>a picture of you that in front of your door</strong> and you are ready to go!</p>
+    <p>After that, you now can buy and request, <b>but nobody can see it!</b> You have to fill other information like <strong>room number</strong> or <strong>a picture in front of your door (and with you, it's even better!)</strong> and you are ready to go!</p>
     <p>Now you are in the system. You can find stuffs you want to buy or create a new one(but make sure make it clear enough that everyone can understand)</p>
     <p>If you want to buy more than 5 stuffs, you HAVE TO GO BY YOURSELF, and don't forget to take some others' order with you and gain some points. (Limit amount might change in the future)</p>
     <p>One point will help you to buy one stuff. 10 points 10 stuffs.</p>
